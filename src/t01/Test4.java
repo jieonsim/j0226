b1535a1952c2eab4638dbfcef82ba6f0fff06669
jieonsim <wldjs5209@gmail.com>
2024-02-29 18:29:02 +0900

    20240229_1828 j0226

---
+++ src/t01/Test4.java
@@ -0,0 +1,19 @@
+package t01;
+
+public class Test4 {
+	// main 메소드
+	public static void main(String[] args) {
+		// void : 반환 타입(리턴값)이 없을 때 씀 
+		// 콘솔에 내용 출력 : sysout + ctrl + space + enter
+		System.out.println("1. 반갑\t습니다."); //\t는 탭, \n은 엔터(줄바꾸기)
+		System.out.print("2. 반갑습니다.\n\n");
+		System.out.println("3. 반갑습니다.");
+		
+		/*
+			이 곳은 여러 줄의 주석 처리
+			여러줄의 설명을 기술할 수 있습니다.
+		 */
+		
+		System.out.println("4. 반갑습니다.");
+	}
+}
